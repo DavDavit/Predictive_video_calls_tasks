fdca1b6237f11a01af289643c71bcde7a99b3880
DavDavit <161076714+DavDavit@users.noreply.github.com>
2024-02-24 00:26:32 +0400

    Create ShowHide_script.js

---
+++ Day2/ShowHide/ShowHide_script.js
@@ -0,0 +1,11 @@
+const toggleButton = document.getElementById('toggleButton');
+const myDiv = document.getElementById('myDiv');
+
+toggleButton.addEventListener('click', function() {
+
+  if (myDiv.style.display === 'none') {
+    myDiv.style.display = 'block';
+  } else {
+    myDiv.style.display = 'none';
+  }
+});
