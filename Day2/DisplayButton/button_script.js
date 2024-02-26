cebedbe4e61e48e53fc41670a801128abba63712
DavDavit <161076714+DavDavit@users.noreply.github.com>
2024-02-26 22:47:07 +0400

    Create button_script.js

---
+++ Day2/DisplayButton/button_script.js
@@ -0,0 +1,3 @@
+document.getElementById('displayButton').addEventListener('click', function() {
+    document.getElementById('displayText').textContent = document.getElementById('inputField').value;
+});
